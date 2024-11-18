aa5dae7169e0ba490f95de0bba6e2200412f7800
Ari Wiradana <ariwiradana5@gmail.com>
2024-11-19 02:50:34 +0800

    modified:   components/elements/button.swiper.actions.tsx

--- components/elements/button.swiper.actions.tsx
+++ components/elements/button.swiper.actions.tsx
@@ -22,10 +22,8 @@ const ButtonSwiperAction: FC<ButtonSwiperActionProps> = ({
         action === "next" ? swiper.slideNext() : swiper.slidePrev()
       }
       className={`w-8 h-8 rounded-full ${syne.className} ${
-        disabled
-          ? "pointer-events-none opacity-40"
-          : "hover:bg-white/40 hover:text-white"
-      } flex items-center justify-center bg-white text-dark rounded-full  ring-0 border-none hover:backdrop-blur transition-all outline-none ease-in-out duration-500 text-lg`}
+        disabled && "pointer-events-none opacity-40"
+      } flex items-center justify-center bg-white text-dark rounded-full ring-0 border-none transition-all outline-none ease-in-out duration-500 text-lg`}
       {...props}
     >
       {icon}
